3968d247414caffb83aefaf6803b7f9d4388fe1d
Valdir Caze Silva Netto <valdir.netto@unimedjp.corp>
2025-09-15 15:47:37 -0300

    docs: preparando para deploy

--- src/conexao.php
+++ src/conexao.php
@@ -1,13 +1,12 @@
 <?php
-
-$host = getenv('DB_HOST') ?: 'db';
-$db_name = getenv('DB_NAME') ?: 'forumazula_db';
-$username = getenv('DB_USER') ?: 'root';
-$password = getenv('DB_PASSWORD') ?: 'root';
+$host = getenv('DB_HOST') ?: 'localhost';
+$db_name = getenv('DB_NAME') ?: 'forumazuladb';
+$username = getenv('DB_USER') ?: 'forumazuladb_user';
+$password = getenv('DB_PASSWORD') ?: '';
 
 try {
     $pdo = new PDO(
-        "mysql:host=$host;dbname=$db_name;charset=utf8mb4",
+        "pgsql:host=$host;port=5432;dbname=$db_name;",
         $username,
         $password
     );
